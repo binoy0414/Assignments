7ce33313555b5b4867499bc36fa264e7312c62b7
binoy0414 <binoy0414@gmail.com>
2025-10-29 09:17:58 +0530

    Hello World

---
+++ Hello.java
@@ -0,0 +1,23 @@
+package Training;
+import java.io.PrintWriter;
+import java.util.logging.*;
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Automation Training.");
+        System.out.printf("Name: %s, Age: %d%n", "Binoy", 42);
+     // → Output: Name: Binoy, Age: 42
+        System.err.println("Error: Something went wrong!");
+        PrintWriter writer = new PrintWriter(System.out, true);
+        writer.println("Hello via PrintWriter");
+        writer.printf("Pi ≈ %.2f%n", Math.PI);
+        Logger logger = Logger.getLogger("MyApp");
+        logger.info("App started");
+        logger.warning("Low disk space");
+        System.out.format("Speed: %.2f km/h%n", 88.567);
+        String msg = String.format("User %s has %d points", "Binoy", 1200);
+        System.out.println(msg);
+
+    }
+}
+
