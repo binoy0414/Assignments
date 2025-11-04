0fa6545b4d66caa0c8678f7c0c56f49b309115ab
BINOY DEBNATH <70596655+binoy0414@users.noreply.github.com>
2025-11-04 07:26:40 +0530

    Add files via upload

---
+++ Assignment1/CurrentYearDataType.java
@@ -0,0 +1,11 @@
+package language.basics;
+
+public class CurrentYearDataType {
+    public static void main(String[] args) {
+        // Data Type: int (for a whole, non-decimal number)
+        int currentYear = 2023;
+
+        System.out.println("Value: " + currentYear);
+        System.out.println("Data Type: " + ((Object)currentYear).getClass().getSimpleName());
+    }
+}
---
+++ Assignment1/OccupationDataType.java
@@ -0,0 +1,11 @@
+package language.basics;
+
+public class OccupationDataType {
+    public static void main(String[] args) {
+        // Data Type: String (for descriptive text)
+        String occupation = "Software Engineer";
+
+        System.out.println("Value: " + occupation);
+        System.out.println("Data Type: " + occupation.getClass().getSimpleName());
+    }
+}
