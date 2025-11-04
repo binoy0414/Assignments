d22c8712b2b19c550164be691719989bf9f30606
BINOY DEBNATH <70596655+binoy0414@users.noreply.github.com>
2025-11-04 07:26:40 +0530

    Add files via upload

---
+++ Assignment1/FollowersDataType.java
@@ -0,0 +1,11 @@
+package language.basics;
+
+public class FollowersDataType {
+    public static void main(String[] args) {
+        // Data Type: int (since followers are counted in whole numbers)
+        int numberOfFollowers = 1000000;
+
+        System.out.println("Value: " + numberOfFollowers);
+        System.out.println("Data Type: " + ((Object)numberOfFollowers).getClass().getSimpleName());
+    }
+}
